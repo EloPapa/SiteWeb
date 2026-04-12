d6f6d048cd791be4104b3790924587c53e270db0
EloPapa <ericbergeron2000@gmail.com>
2026-04-12 14:36:11 -0400

    test

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -27,7 +27,7 @@ const Footer = ({}) => {
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
         <Link href="http://www.eloise-bergeron.vercel.app">
-          <h1 className="underline underline-offset-1">Eloĩse Bergeron</h1>
+          <h1 className="underline underline-offset-1">Eloĩse</h1>
         </Link>
       </h1>
     </>
