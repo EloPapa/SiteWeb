11b4eb5b78f4217c0a8aff55ab5d99b3ff1163ed
EloPapa <ericbergeron2000@gmail.com>
2026-04-28 10:59:34 -0400

    m

--- src/app/page.js
+++ src/app/page.js
@@ -100,7 +100,7 @@ export default function Home() {
 
           {/* 🐵 MÉDAILLON SINGE (HAUT DE PAGE) */}
             <div
-              className="absolute left-[38%] top-[1%] z-40
+              className="absolute left-[40%] top-[1%] z-40
                         w-[35px] h-[35px]
                         sm:w-[120px] sm:h-[120px]
                         md:w-[160px] md:h-[160px]
