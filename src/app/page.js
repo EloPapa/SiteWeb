618c4e1bc3f22ff70b4a9c15d6cba7e4c8cb569c
EloPapa <ericbergeron2000@gmail.com>
2026-04-28 10:44:30 -0400

    m

--- src/app/page.js
+++ src/app/page.js
@@ -86,7 +86,7 @@ export default function Home() {
                       sm:w-[180px] sm:h-[180px]
                       md:w-[260px] md:h-[260px]
                       lg:w-[800px] lg:h-[800px]
-                      rounded-full overflow-hidden"
+                      rounded-full"
           >
             <img
               src="/images/elo/labradorPetite.png"
