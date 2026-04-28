3e8098f5b519695347561511396edd51d2f8311c
EloPapa <ericbergeron2000@gmail.com>
2026-04-28 11:27:24 -0400

    test

--- src/app/page.js
+++ src/app/page.js
@@ -81,7 +81,7 @@ export default function Home() {
 
 {/* 🐶 MÉDAILLON CHIEN (RESPONSIVE) */}
 <div
-  className="fixed right-[5%] top-[8%] z-50
+  className="fixed right-[5%] top-[15%] z-50
             w-[120px] h-[120px]
             sm:w-[180px] sm:h-[180px]
             md:w-[320px] md:h-[320px]
