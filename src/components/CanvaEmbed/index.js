fba45d73b6d7244767b974a91b19780347a3a507
EloPapa <ericbergeron2000@gmail.com>
2026-04-28 07:01:07 -0400

    test

--- src/components/CanvaEmbed/index.js
+++ src/components/CanvaEmbed/index.js
@@ -7,7 +7,7 @@ export default function CanvaEmbed() {
         position: "relative",
         width: "100%",
         height: 0,
-        paddingTop: "133.3333%",
+        paddingTop: "0%",
         boxShadow: "0 2px 8px rgba(63,69,81,0.16)",
         marginTop: "0em",
         marginBottom: "0em",
