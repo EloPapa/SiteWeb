6e4904fc1f6fe686a8b49284e6eac0d99fcc4ccf
EloPapa <ericbergeron2000@gmail.com>
2026-04-28 06:11:35 -0400

    Add CanvaEmbed component

---
+++ src/components/CanvaEmbed/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function CanvaEmbed() {
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        height: 0,
+        paddingTop: "133.3333%",
+        boxShadow: "0 2px 8px rgba(63,69,81,0.16)",
+        marginTop: "1.6em",
+        marginBottom: "0.9em",
+        overflow: "hidden",
+        borderRadius: "8px",
+      }}
+    >
+      <iframe
+        loading="lazy"
+        src="https://www.canva.com/design/DAHIEVkSkZc/ofmY5fjC9RyOVqU1AepXZg/view?embed"
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          border: "none",
+        }}
+        allowFullScreen
+        title="Canva design"
+      />
+    </div>
+  );
+}
