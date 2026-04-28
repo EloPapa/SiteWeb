ad273ac1bd40ea0c1b5440d1d301a10bfbeedb19
EloPapa <ericbergeron2000@gmail.com>
2026-04-28 06:03:13 -0400

    debug

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ import Footer from "../components/Footer";
 import Button from "../components/Button";
 import Socials from "../components/Socials";
 import WorkCard from "../components/WorkCard";
-import CanvaEmbed from "../components/Canva"
+const CanvaEmbed = dynamic(() => import("../components/CanvaEmbed"), { ssr: false });
 
 import data from "../data/portfolio.json";
 import { useLanguage } from "../context/LanguageContext";
