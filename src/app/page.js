c3fede4bf9c95ea2dafef27e3b2abb7b3d837392
EloPapa <ericbergeron2000@gmail.com>
2026-05-20 14:13:12 -0400

    t

--- src/app/page.js
+++ src/app/page.js
@@ -96,9 +96,9 @@ export default function Home() {
 
       {/* 🐶 MÉDAILLON CHIEN */}
       <div
-        className="absolute right-[5%] top-[10%] z-10
-                  xl:right-[10%] xl:top-[5%]
-                  2xl:right-[30%] 2xl:top-[5%]
+        className="absolute right-[5%] top-[8%] z-10
+                  xl:right-[10%] xl:top-[3%]
+                  2xl:right-[30%] 2xl:top-[3%]
                   w-[120px] h-[120px]
                   sm:w-[180px] sm:h-[180px]
                   md:w-[280px] md:h-[280px]
