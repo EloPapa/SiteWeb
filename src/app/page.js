8c6d0cba17d6d2216b945bbdb4be7c6874b56e26
EloPapa <ericbergeron2000@gmail.com>
2026-05-20 13:57:37 -0400

    t

--- src/app/page.js
+++ src/app/page.js
@@ -122,17 +122,17 @@ export default function Home() {
 
       <div className="container mx-auto">
         {/* SECTION ACCUEIL/PRÉSENTATION */}
-        <div className="mt-0">
-          <h1 ref={textOne} className="text-3xl laptop:text-4xl py-1.5 px-2">
+        <div className="mt-0 p-2">
+          <h1 ref={textOne} className="text-3xl laptop:text-4xl py-1.5">
             {taglines[0]}
           </h1>
-          <h1 ref={textTwo} className="text-3xl laptop:text-4xl p-1.5 px-2">
+          <h1 ref={textTwo} className="text-3xl laptop:text-4xl p-1.5">
             {taglines[1]}
           </h1>
-          <h1 ref={textThree} className="text-3xl laptop:text-4xl p-1.5 px-2">
+          <h1 ref={textThree} className="text-3xl laptop:text-4xl p-1.5">
             {taglines[2]}
           </h1>
-          <h1 ref={textFour} className="text-3xl laptop:text-4xl p-1.5 px-2">
+          <h1 ref={textFour} className="text-3xl laptop:text-4xl p-1.5">
             {taglines[3]}
           </h1>
 
